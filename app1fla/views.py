e8316d4349c46822718e3389b8270bfb243915e3
Flaviu Maxim <flaviomaxim@gmail.com>
2024-05-30 22:11:38 +0200

    final

--- app1fla/views.py
+++ app1fla/views.py
@@ -1,5 +1,7 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+
+from .forms import ProductoForm
 from .models import Producto
 
 # Create your views here.
@@ -21,3 +23,20 @@ def detalle(request, producto_id):
         request,
         'detalle.html',
         context={'producto': producto})
+
+
+def formulario(request):
+    if request.method == 'POST':
+
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/app1fla')
+    else:
+        form = ProductoForm()
+
+    return render(
+        request,
+        'producto_form.html',
+        {'form': form}
+    )
--- flamax/settings.py
+++ flamax/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from django.forms.renderers import TemplatesSetting
+
+
+class CustomFormRendered(TemplatesSetting):
+    form_template_name = 'form_snippet.html'
+
+
+FORM_RENDERER = "flamax.settings.CustomFormRendered"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.forms',
     'app1fla.apps.App1FlaConfig',
 ]
 
